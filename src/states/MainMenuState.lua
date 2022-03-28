e79bd74e449fa13457649938eeaaf3d911e07033
Arthur's Code <69600183+Arthurs-code@users.noreply.github.com>
2022-03-28 21:36:31 +0200

    >tries to help >fails MISERABLY

--- src/states/MainMenuState.lua
+++ src/states/MainMenuState.lua
@@ -1,5 +1,7 @@
 local mainmenu = {}
 
+local menuItems = { }
+
 local options = {"story_mode", "freeplay", "donate", "options"}
 local curSelected = 1
 
@@ -8,18 +10,30 @@ local function changeSelection(change)
 
     if curSelected > #options then curSelected = 1 end
     if curSelected < 1 then curSelected = #options end
+
 end
 
 function mainmenu.load()
     menuBG = paths.getImage("menuBG")
     _c.add(menuBG)
-
+    for i = 1, #options do
+        spr = sprite.new(paths.atlas("mainmenu/menu_" .. options[i]),
+                                        love.graphics.getWidth() / 4, 50 + (i - 1) * 155)
+        spr:addAnim("idle", options[i] .. " basic")
+        spr:addAnim("idle", options[i] .. " white")
+    end
+    spr:playAnim("idle")
     changeSelection(0)
 end
 
-function mainmenu.update(dt) end
+function mainmenu.update(dt) 
+    spr:update(dt)
+end
 
-function mainmenu.draw() love.graphics.draw(menuBG, 0, 0, 0, 1.1, 1.1) end
+function mainmenu.draw()
+    love.graphics.draw(menuBG, 0, 0, 0, 1.1, 1.1) 
+    spr:draw()
+end
 
 function mainmenu.keypressed(key, scancode, isrepeat)
     if not isrepeat then
